c00631791d8069ee4a93475cd5b0da33c5a1fb9f
victor <vjbsh@hotmail.com>
2023-01-23 12:53:20 -0600

    #prototype for singleton finished

--- src/main/java/com/mayab/desarrollo/creacional/singleton/Controller.java
+++ src/main/java/com/mayab/desarrollo/creacional/singleton/Controller.java
@@ -2,14 +2,16 @@ package com.mayab.desarrollo.creacional.singleton;
 
 public class Controller {
     public static void main(String[] args) {
+        DBConnect.getInstance();
         ReporteA reporteA = new ReporteA();
         reporteA.print();
-        ReporteB reporteB = new ReporteB();
-        reporteB.print();
         System.out.println(reporteA.update("Test update A"));
         System.out.println(reporteA.delete("Test delete A"));
         System.out.println(reporteA.insert("Test insert A"));
         System.out.println(reporteA.select("Test select A"));
+        DBConnect.close();
+        ReporteB reporteB = new ReporteB();
+        reporteB.print();
         System.out.println(reporteB.update("Test update B"));
         System.out.println(reporteB.delete("Test delete B"));
         System.out.println(reporteB.insert("Test insert B"));

0eaac1762dd2a693de5ee4c69af9fa7f30a06060
victor <vjbsh@hotmail.com>
2023-03-01 15:29:05 -0600

    fixed cliente in adapter

--- src/main/java/com/mayab/desarrollo/estructural/adapter/Cliente.java
+++ src/main/java/com/mayab/desarrollo/estructural/adapter/Cliente.java
@@ -1,14 +1,21 @@
 package com.mayab.desarrollo.estructural.adapter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente {
     public static void main(String[] args){
         DeptB deptB = new DeptB("Victor", "Test", LocalDate.parse("2000-12-01"));
-        DeptA deptA = new DeptA("Victor Test", 21);
-        deptA.print();
-        AdaptA adaptA = new AdaptA(deptB);
-        adaptA.print();
+        Dept deptA = new DeptA("Victor Test", 21);
+        Dept adaptA = new AdaptA(deptB);
+        List<Dept> list = new ArrayList<Dept>();
+        list.add(deptA);
+        list.add(adaptA);
+        for (int i=0; i< list.size();i++){
+            System.out.println("-------Persona---" +(i+1)+ "------");
+            list.get(i).print();
+        }
 
     }
 }
